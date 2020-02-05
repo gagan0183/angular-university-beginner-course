82d4d494b8b86f86b89adb9877fc64a6159c7cd5
Gagandeep singh <g.deepsingh1@gmail.com>
2020-02-05 21:43:12 +0530

    component inputs and @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
-
+import { Course } from './model/course';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-
-
+  angularCore: Course = COURSES[0];
 }
